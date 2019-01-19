0a22269282399edf1580dad4c493061501fb630c
Space Edge Lab <tshamada@users.noreply.github.com>
2019-01-19 19:23:41 +0900

    update logo

--- backend/src/controllers/img.js
+++ backend/src/controllers/img.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
   console.log(ts() + JSON.stringify(req.query));
 
   if ('logo' === req.query.target) {
-    return res.sendFile(`${process.cwd()}/src/resources/logo.png`);
+    return res.sendFile(`${process.cwd()}/src/resources/TRICOMIFY.svg.png`);
   }
 
   return res.sendFile(`${process.cwd()}/src/resources/404-notFound.png`);
