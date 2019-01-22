aa05e48dea70431966917cdde995948122d8d4d7
Space Edge Lab <tshamada@users.noreply.github.com>
2019-01-22 09:50:41 +0900

    update app/openlayers

--- app/openlayers/index.js
+++ app/openlayers/index.js
@@ -1,17 +1,22 @@
 import 'ol/ol.css';
-import {Map, View} from 'ol';
+import { Map, View } from 'ol';
+import { Tile, Image, Vector } from 'ol/layer';
 import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
 import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+
 
 const map = new Map({
   target: 'map',
   layers: [
-    new TileLayer({
+    new Tile({
       source: new OSM()
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 0
+    projection: 'EPSG:3857', //HERE IS THE VIEW PROJECTION
+    center: fromLonLat([139.754784, 35.708316]),
+    zoom: 20
   })
 });
