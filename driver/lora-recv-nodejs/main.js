565d790a72366e1d749f876af4e3df993c369b58
Space Edge Lab <tshamada@users.noreply.github.com>
2019-01-23 13:07:29 +0900

    update driver for nodejs

---
+++ driver/lora-recv-nodejs/main.js
@@ -0,0 +1,16 @@
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/ttyUSB0', {
+  baudRate: 57600
+});
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+const ts = () => ('['+String(new Date()).substr(0, 24)+']: ');
+const debug = msg => (console.log(ts() + msg));
+
+parser.on('data', data => {
+  const s = data.replace(/\r/g, '');
+  debug(s);
+});
+
+
