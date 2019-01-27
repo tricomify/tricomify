33bec416628e9c89df8a88a750c49b930b0283aa
Space Edge Lab <tshamada@users.noreply.github.com>
2019-01-27 09:34:01 +0900

    fix driver

--- driver/lora-recv-nodejs/main.js
+++ driver/lora-recv-nodejs/main.js
@@ -22,7 +22,7 @@ const createPacket = (txmsg) => {
 parser.on('data', data => {
   const s = data.replace(/\r/g, '');
   const { convRaw2Json } = require('./utils/convRaw2Json');
-  const { writeJson } = require('./utils/writePacket');
+  const { writePacket } = require('./utils/writePacket');
 
   const json = convRaw2Json(data);
   
