6d31c32ed3b1a05394736faeba50ce8a4907105d
tshamada <hamada@at-vision.jp>
2019-03-08 02:45:06 +0900

    update app/openlayers/index.js

--- app/openlayers/index.js
+++ app/openlayers/index.js
@@ -151,8 +151,9 @@ const map = new Map({
     // center:	[115.233488, -8.705538], // Bali, TUNA Center
     // center:	[115.263554,-8.695564], // Bali, Maison Hotel
     // center:	[115.262592, -8.707222], // Bali, Beach
-    center:	[30.094826, -1.95233900000], // Kigali
+    // center:	[30.094826, -1.95233900000], // Kigali Mariot Hotel
+    center:	[30.099555, -1.955555], // Kigali City Center
 
-    zoom: 13
+    zoom: 14.5
   })
 });
