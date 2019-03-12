ffde0e4b80bda42f7cd7247d9b550e285e254443
tshamada <hamada@spacelab.site>
2019-03-13 08:43:41 +0900

    fix utils/writePacket.js for driver

--- driver/lora-recv-nodejs/utils/writePacket.js
+++ driver/lora-recv-nodejs/utils/writePacket.js
@@ -22,8 +22,9 @@ module.exports.writePacket = (packet) => {
   const dd = String((da.getDate())).padStart(2, '0');
   const hh = String((da.getHours())).padStart(2, '0');
   const mi = String((da.getMinutes())).padStart(2, '0');
+  const ss = String((da.getSeconds())).padStart(2, '0');
   const ms = String((da.getMilliseconds())).padStart(3, '0');
-  const fname = './log/' + yy + mm + dd + '-' + hh + mi + '-' + ms + '.json'; 
+  const fname = './log/' + yy + mm + dd + '-' + hh + mi + '-' + ss + '-' + ms + '.json'; 
 
   saveFile(fname, JSON.stringify(packet, undefined, 2));
 
