04a7713426db369c8199ff1d35b8507599d232a2
tshamada <hamada@spacelab.site>
2019-03-16 19:25:34 +0900

    update center.js

--- app/openlayers/center.js
+++ app/openlayers/center.js
@@ -6,10 +6,17 @@ export const center = [
   // 115.262592, -8.707222 // Bali, Beach
   // 30.094826, -1.952339  // Kigali Mariot Hotel
   // 30.099555, -1.955555  // Kigali City Center
-  // 139.760912, 35.545352  // Haneda Airport
-  // 139.7455, 35.6586  // Tokyo Tower
+  // 139.760912, 35.545352 // Haneda Airport
+  // 139.7455, 35.6586     // Tokyo Tower
   // 139.765312, 35.716048 // Tokyo Asano Campus
-  // 139.7605, 35.7093 // U-Tokyo, Akamon
-  // 139.761376, 35.72118 // Tokyo, Sendagi 2chome BusStop
-  139.7774, 35.7065 // Tokyo, NanjaMonja Tree
+  // 139.7605, 35.7093     // U-Tokyo, Akamon
+  // 139.761376, 35.72118  // Tokyo, Sendagi 2chome BusStop
+  // 139.7774, 35.7065     // Tokyo, NanjaMonja Tree
+  // 79.9, 6.9             // Colombo
+  // 140.39208, 35.744152  // Narita Airport
+  // 140.464256, 38.555524 // sendai
+  // 141.5137, 42.9419     // Eniwa, Sapporo 
+  // 141.491392, 43.069092 // Nopporo
+  // 139.770864, 35.70924  // yushima
+  139.734784, 35.69178 // TKP Center, Ichigaya
 ]
