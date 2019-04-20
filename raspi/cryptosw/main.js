95d46e7eae24d1e82726cb2d83d3d4796c36e7f6
tshamada <hamada@spacelab.site>
2019-04-21 00:27:15 +0900

    update raspi: qrcode-console

--- raspi/cryptosw/main.js
+++ raspi/cryptosw/main.js
@@ -2,12 +2,13 @@
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const tstr = () => (String(new Date()).substr(0, 24));
 const gpio = require('rpi-gpio');
+const qrcode = require( 'qrcode-console' );
 
 const pinGpio = 40; // Pin# 40（GPIO21, not GPIO40)
 const CryptoAuth = require('./utils/cryptoAuth');
 const auth = new CryptoAuth();
 
-const ndelay = 5000; // msec
+const ndelay = 1000; // msec
 let balancePrev = auth.updateBalance();
 console.log(tstr(), balancePrev);
 
@@ -59,13 +60,43 @@ const mainLoop = () => {
 
       const meter = jukeBox.getMeter();
 
+      const black   = '\u001b[30m';
+      const red     = '\u001b[31m';
+      const green   = '\u001b[32m';
+      const yellow  = '\u001b[33m';
+      const blue    = '\u001b[34m';
+      const magenta = '\u001b[35m';
+      const cyan    = '\u001b[36m';
+      const white   = '\u001b[37m';
+      const reset   = '\u001b[0m';
+
+      const escClearScreen = '\u001b[2J';  // clear screen
+      const escMoveUp = '\u001b[250A'; // move up 250 lines
+      console.log(escClearScreen + escMoveUp); 
+
       if(meter > 1) {
-        console.log('---------------------- ON');
         gpio.write(pinGpio, true);
+        console.log(yellow);
+        qrcode.generate('0x5b8dCE0667C0e28F11E2Fa544d78A771cE8AE60B');
+        console.log('');
+        console.log('status: ON');
+        console.log(reset);
       } else {
-        console.log('...................... OFF');
         gpio.write(pinGpio, false);
+        console.log(red);
+        qrcode.generate('0x5b8dCE0667C0e28F11E2Fa544d78A771cE8AE60B');
+        console.log('');
+        console.log('status: OFF');
+        console.log(reset);
       }
+
+      if (meter > 1 ) {
+        console.log(cyan + meter/1.0e3 + reset + ' sec');
+      } else {
+        console.log(white + meter/1.0e3 + reset + ' sec');
+      }
+      console.log('');
+
 
       console.log("status: %o", {
         time: tstr(),
@@ -76,6 +107,7 @@ const mainLoop = () => {
         timer: jukeBox.getTimer(),
         meter: jukeBox.getMeter()/1.0e3,
       });
+
 
       balancePrev = balance;
     }, ndelay);
